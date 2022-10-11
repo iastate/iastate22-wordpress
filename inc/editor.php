aa0565f24da71dd7bcb08561fdb43ffaf361cf29
Dan Rogers <dan.rogers@idfive.com>
2022-10-11 07:37:40 -0600

    comment out gutenburg js, dont think its required.

--- inc/editor.php
+++ inc/editor.php
@@ -7,7 +7,12 @@ function _gutenberg_css(){
 	add_editor_style( '/wp_components/build/css/editor.css' ); // tries to include style-editor.css directly from your theme folder
 }
 
-add_action('enqueue_block_editor_assets', '_gutenberg_js');
+//add_action('enqueue_block_editor_assets', '_gutenberg_js');
+
+//function _gutenberg_js() {
+//    wp_register_script('editor-js', get_stylesheet_directory_uri() . '/vendor/iastate/frontend-component-library/build/js/editor.js', array(), '', true);
+//    wp_enqueue_script('editor-js');
+//}
 
 function customWYSIWYG($arr){
 	$arr['block_formats'] = 'Paragraph=p;Heading 2=h2;Heading 3=h3;Heading 4=h4';
