fbc1a520c1e541ff15c23a230ec6249369a317af
Mark Levy <mark.levy@idfive.com>
2023-05-30 16:56:57 -0400

    Removal of 'Advanced Custom Search' function. Remains commented out for now, but conflicts with the API

--- inc/search.php
+++ inc/search.php
@@ -142,69 +142,71 @@ function list_searcheable_acf(){
  * see https://vzurczak.wordpress.com/2013/06/15/extend-the-default-wordpress-search/
  * credits to Vincent Zurczak for the base query structure/spliting tags section
  */
-function advanced_custom_search( $where, &$wp_query ) {
+// INCOMPATABLE WITH WP-JSON
 
-    global $wpdb;
+// function advanced_custom_search( $where, &$wp_query ) {
+
+//     global $wpdb;
  
-    if ( empty( $where ))
-        return $where;
+//     if ( empty( $where ))
+//         return $where;
  
-    // get search expression
-    $terms = $wp_query->query_vars[ 's' ];
+//     // get search expression
+//     $terms = $wp_query->query_vars[ 's' ];
     
-    // explode search expression to get search terms
-    $exploded = explode( ' ', $terms );
-    if( $exploded === FALSE || count( $exploded ) == 0 )
-        $exploded = array( 0 => $terms );
+//     // explode search expression to get search terms
+//     $exploded = explode( ' ', $terms );
+//     if( $exploded === FALSE || count( $exploded ) == 0 )
+//         $exploded = array( 0 => $terms );
          
-    // reset search in order to rebuilt it as we whish
-    $where = '';
+//     // reset search in order to rebuilt it as we whish
+//     $where = '';
     
-    // get searcheable_acf, a list of advanced custom fields you want to search content in
-    $list_searcheable_acf = list_searcheable_acf();
+//     // get searcheable_acf, a list of advanced custom fields you want to search content in
+//     $list_searcheable_acf = list_searcheable_acf();
 
-    foreach( $exploded as $tag ) :
-        $where .= " 
-          AND (
-            (wp_posts.post_title LIKE '%$tag%')
-            OR (wp_posts.post_content LIKE '%$tag%')
-            OR EXISTS (
-              SELECT * FROM wp_postmeta
-	              WHERE post_id = wp_posts.ID
-	                AND (";
+//     foreach( $exploded as $tag ) :
+//         $where .= " 
+//           AND (
+//             (wp_posts.post_title LIKE '%$tag%')
+//             OR (wp_posts.post_content LIKE '%$tag%')
+//             OR EXISTS (
+//               SELECT * FROM wp_postmeta
+// 	              WHERE post_id = wp_posts.ID
+// 	                AND (";
 
-        foreach ($list_searcheable_acf as $searcheable_acf) :
-          if ($searcheable_acf == $list_searcheable_acf[0]):
-            $where .= " (meta_key LIKE '%" . $searcheable_acf . "%' AND meta_value LIKE '%$tag%') ";
-          else :
-            $where .= " OR (meta_key LIKE '%" . $searcheable_acf . "%' AND meta_value LIKE '%$tag%') ";
-          endif;
-        endforeach;
+//         foreach ($list_searcheable_acf as $searcheable_acf) :
+//           if ($searcheable_acf == $list_searcheable_acf[0]):
+//             $where .= " (meta_key LIKE '%" . $searcheable_acf . "%' AND meta_value LIKE '%$tag%') ";
+//           else :
+//             $where .= " OR (meta_key LIKE '%" . $searcheable_acf . "%' AND meta_value LIKE '%$tag%') ";
+//           endif;
+//         endforeach;
 
-	        $where .= ")
-            )
-            OR EXISTS (
-              SELECT * FROM wp_comments
-              WHERE comment_post_ID = wp_posts.ID
-                AND comment_content LIKE '%$tag%'
-            )
-            OR EXISTS (
-              SELECT * FROM wp_terms
-              INNER JOIN wp_term_taxonomy
-                ON wp_term_taxonomy.term_id = wp_terms.term_id
-              INNER JOIN wp_term_relationships
-                ON wp_term_relationships.term_taxonomy_id = wp_term_taxonomy.term_taxonomy_id
-              WHERE (
-          		taxonomy = 'post_tag'
-            		OR taxonomy = 'category'          		
-            		OR taxonomy = 'myCustomTax'
-          		)
-              	AND object_id = wp_posts.ID
-              	AND wp_terms.name LIKE '%$tag%'
-            )
-        )";
-    endforeach;
-    return $where;
-}
+// 	        $where .= ")
+//             )
+//             OR EXISTS (
+//               SELECT * FROM wp_comments
+//               WHERE comment_post_ID = wp_posts.ID
+//                 AND comment_content LIKE '%$tag%'
+//             )
+//             OR EXISTS (
+//               SELECT * FROM wp_terms
+//               INNER JOIN wp_term_taxonomy
+//                 ON wp_term_taxonomy.term_id = wp_terms.term_id
+//               INNER JOIN wp_term_relationships
+//                 ON wp_term_relationships.term_taxonomy_id = wp_term_taxonomy.term_taxonomy_id
+//               WHERE (
+//           		taxonomy = 'post_tag'
+//             		OR taxonomy = 'category'          		
+//             		OR taxonomy = 'myCustomTax'
+//           		)
+//               	AND object_id = wp_posts.ID
+//               	AND wp_terms.name LIKE '%$tag%'
+//             )
+//         )";
+//     endforeach;
+//     return $where;
+// }
  
-add_filter( 'posts_search', 'advanced_custom_search', 500, 2 );
+// add_filter( 'posts_search', 'advanced_custom_search', 500, 2 );
