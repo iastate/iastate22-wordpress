eeed98ac496e96ad7dc80d2e70ffa6945ecbf26e
Mark Levy <mark.levy@idfive.com>
2023-08-09 14:33:18 -0400

    Permalink reset patch added to overrides file

--- inc/overrides.php
+++ inc/overrides.php
@@ -14,3 +14,27 @@ if (!function_exists('acf_register_block_type')){
 		return false;
 	}
 }
+
+/**
+ * Look for $_GET variable set if a post type was toggled.
+ * If criteria are met then flush rewrite rules.
+ *
+ * @wp-hook load-toplevel_page_acf-options should only load in the ACF theme option menu.
+ *
+ * @return void
+ */
+function acf_maybe_refresh_rewrite_rules_for_post_type_options() {
+  if ( true === filter_input( INPUT_GET, 'acf_post_type_refresh', FILTER_VALIDATE_BOOLEAN ) ) {
+      if ( false !== wp_verify_nonce( filter_input( INPUT_GET, 'refresh_nonce', FILTER_SANITIZE_STRING ), 'refresh_rewrite_after_post_type_change' ) ) {
+          flush_rewrite_rules( false );
+          if ( wp_safe_redirect( remove_query_arg( array(
+              'acf_post_type_refresh',
+              'refresh_nonce'
+          ) ) ) ) {
+              exit();
+          }
+      }
+  }
+}
+
+add_action( 'load-toplevel_page_acf-options', 'acf_maybe_refresh_rewrite_rules_for_post_type_options' );
