11d1947af7f84aaa6c4ada33349567c65ceff371
Kevin Wickham <kwickham@iastate.edu>
2023-11-08 13:19:20 -0600

    post show author fix

--- single.php
+++ single.php
@@ -16,16 +16,6 @@ $context['post'] = $timber_post;
 if ( $multipage ) {
     $context['post'] -> post_content = $pages[ $page - 1 ];
 }
-
-$author_info = get_post_meta( $timber_post->ID, '_meta_info', true) ? get_post_meta( $timber_post->ID, '_meta_info', true) : 1;
-
-if ($author_info == 'show_author' || $author_info == 1) {
-	$context['show_author'] = true;
-} else {
-	$context['show_author'] = false;
-}
-
-
 if ( post_password_required( $timber_post->ID ) ) {
 	Timber::render( 'single-password.twig', $context );
 } else {
